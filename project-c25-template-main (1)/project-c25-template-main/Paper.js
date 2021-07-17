2cca342517f63819710fe787eabd0b8d3d60fba3
Maheswar3 <84121418+Maheswar3@users.noreply.github.com>
2021-07-17 13:57:10 +0400

    Add files via upload

---
+++ project-c25-template-main (1)/project-c25-template-main/Paper.js
@@ -0,0 +1,33 @@
+class paper  {
+
+constructor (x,y,width,height)
+{
+var options = {
+isStatic:false,
+restitution:0.3,
+friction:0,
+density:1.2,
+}
+this.body = Bodies.rectangle (x,y,50,50,options);
+this.width- 40 ;
+this.height - 50;
+this.image=loadImage("paper.png");
+World.add (world,this.body);
+}
+
+display (){
+var pos = this.body.position;
+pos.x=mouseX;
+pos.y=mouseY;
+var angle =  this.body.angle;
+
+push ();
+translate (pos.x,pos.y);
+rotate (angle);
+strokeWeight (3);
+
+imageMode (CENTER)
+rect (0,0,this,width,this.height);
+pop ();
+}
+}
